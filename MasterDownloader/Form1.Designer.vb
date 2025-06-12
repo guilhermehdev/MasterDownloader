4573ddded005532edccb5dee8a4a73c334244e10
guilherme <guilhontina@hotmail.com>
2025-06-12 19:01:34 -0300

    Adicionar arquivos de projeto.

---
+++ MasterDownloader/Form1.Designer.vb
@@ -0,0 +1,230 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    Private components As System.ComponentModel.IContainer
+
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        txtUrl = New TextBox()
+        btnAdicionar = New Button()
+        btnExecutar = New Button()
+        txtLog = New TextBox()
+        lstLinks = New ListBox()
+        btLimparLista = New Button()
+        progressBarDownload = New ProgressBar()
+        CheckBoxAudio = New CheckBox()
+        StatusStrip1 = New StatusStrip()
+        btLog = New Button()
+        chkLegendas = New CheckBox()
+        MenuStrip1 = New MenuStrip()
+        ConfoguraçõesToolStripMenuItem = New ToolStripMenuItem()
+        ImportarCookiesPrivadosToolStripMenuItem = New ToolStripMenuItem()
+        AlterarPastaDestinoToolStripMenuItem = New ToolStripMenuItem()
+        btCancelar = New Button()
+        MenuStrip1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' txtUrl
+        ' 
+        txtUrl.Location = New Point(12, 30)
+        txtUrl.Name = "txtUrl"
+        txtUrl.PlaceholderText = "URL do vídeo ou playlist"
+        txtUrl.Size = New Size(440, 23)
+        txtUrl.TabIndex = 0
+        ' 
+        ' btnAdicionar
+        ' 
+        btnAdicionar.BackColor = Color.YellowGreen
+        btnAdicionar.Cursor = Cursors.Hand
+        btnAdicionar.FlatAppearance.BorderSize = 0
+        btnAdicionar.FlatStyle = FlatStyle.Flat
+        btnAdicionar.ForeColor = Color.White
+        btnAdicionar.Location = New Point(458, 30)
+        btnAdicionar.Name = "btnAdicionar"
+        btnAdicionar.Size = New Size(45, 23)
+        btnAdicionar.TabIndex = 2
+        btnAdicionar.Text = "Add"
+        btnAdicionar.UseVisualStyleBackColor = False
+        ' 
+        ' btnExecutar
+        ' 
+        btnExecutar.BackColor = Color.SteelBlue
+        btnExecutar.Cursor = Cursors.Hand
+        btnExecutar.FlatAppearance.BorderSize = 0
+        btnExecutar.FlatStyle = FlatStyle.Flat
+        btnExecutar.ForeColor = Color.White
+        btnExecutar.Location = New Point(207, 187)
+        btnExecutar.Name = "btnExecutar"
+        btnExecutar.Size = New Size(102, 30)
+        btnExecutar.TabIndex = 3
+        btnExecutar.Text = "Download"
+        btnExecutar.UseVisualStyleBackColor = False
+        ' 
+        ' txtLog
+        ' 
+        txtLog.Location = New Point(12, 222)
+        txtLog.Multiline = True
+        txtLog.Name = "txtLog"
+        txtLog.ReadOnly = True
+        txtLog.ScrollBars = ScrollBars.Vertical
+        txtLog.Size = New Size(491, 176)
+        txtLog.TabIndex = 9
+        txtLog.Visible = False
+        ' 
+        ' lstLinks
+        ' 
+        lstLinks.FormattingEnabled = True
+        lstLinks.ItemHeight = 15
+        lstLinks.Location = New Point(12, 59)
+        lstLinks.Name = "lstLinks"
+        lstLinks.Size = New Size(491, 124)
+        lstLinks.TabIndex = 12
+        ' 
+        ' btLimparLista
+        ' 
+        btLimparLista.BackColor = Color.IndianRed
+        btLimparLista.Cursor = Cursors.Hand
+        btLimparLista.FlatAppearance.BorderSize = 0
+        btLimparLista.FlatStyle = FlatStyle.Flat
+        btLimparLista.ForeColor = Color.White
+        btLimparLista.Location = New Point(413, 187)
+        btLimparLista.Name = "btLimparLista"
+        btLimparLista.Size = New Size(90, 30)
+        btLimparLista.TabIndex = 13
+        btLimparLista.Text = "Limpar lista "
+        btLimparLista.UseVisualStyleBackColor = False
+        ' 
+        ' progressBarDownload
+        ' 
+        progressBarDownload.Location = New Point(12, 404)
+        progressBarDownload.Name = "progressBarDownload"
+        progressBarDownload.Size = New Size(491, 23)
+        progressBarDownload.Style = ProgressBarStyle.Continuous
+        progressBarDownload.TabIndex = 14
+        ' 
+        ' CheckBoxAudio
+        ' 
+        CheckBoxAudio.AutoSize = True
+        CheckBoxAudio.Location = New Point(12, 185)
+        CheckBoxAudio.Name = "CheckBoxAudio"
+        CheckBoxAudio.Size = New Size(100, 19)
+        CheckBoxAudio.TabIndex = 15
+        CheckBoxAudio.Text = "Somente mp3"
+        CheckBoxAudio.UseVisualStyleBackColor = True
+        ' 
+        ' StatusStrip1
+        ' 
+        StatusStrip1.Location = New Point(0, 438)
+        StatusStrip1.Name = "StatusStrip1"
+        StatusStrip1.Size = New Size(516, 22)
+        StatusStrip1.TabIndex = 16
+        StatusStrip1.Text = "StatusStrip1"
+        ' 
+        ' btLog
+        ' 
+        btLog.Cursor = Cursors.Hand
+        btLog.Location = New Point(121, 187)
+        btLog.Name = "btLog"
+        btLog.Size = New Size(80, 30)
+        btLog.TabIndex = 17
+        btLog.Text = "Exibir Log"
+        btLog.UseVisualStyleBackColor = True
+        ' 
+        ' chkLegendas
+        ' 
+        chkLegendas.AutoSize = True
+        chkLegendas.Location = New Point(12, 202)
+        chkLegendas.Name = "chkLegendas"
+        chkLegendas.Size = New Size(108, 19)
+        chkLegendas.TabIndex = 18
+        chkLegendas.Text = "Baixar legendas"
+        chkLegendas.UseVisualStyleBackColor = True
+        ' 
+        ' MenuStrip1
+        ' 
+        MenuStrip1.Items.AddRange(New ToolStripItem() {ConfoguraçõesToolStripMenuItem})
+        MenuStrip1.Location = New Point(0, 0)
+        MenuStrip1.Name = "MenuStrip1"
+        MenuStrip1.Size = New Size(516, 24)
+        MenuStrip1.TabIndex = 19
+        MenuStrip1.Text = "MenuStrip1"
+        ' 
+        ' ConfoguraçõesToolStripMenuItem
+        ' 
+        ConfoguraçõesToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {ImportarCookiesPrivadosToolStripMenuItem, AlterarPastaDestinoToolStripMenuItem})
+        ConfoguraçõesToolStripMenuItem.Name = "ConfoguraçõesToolStripMenuItem"
+        ConfoguraçõesToolStripMenuItem.Size = New Size(96, 20)
+        ConfoguraçõesToolStripMenuItem.Text = "Configurações"
+        ' 
+        ' ImportarCookiesPrivadosToolStripMenuItem
+        ' 
+        ImportarCookiesPrivadosToolStripMenuItem.Name = "ImportarCookiesPrivadosToolStripMenuItem"
+        ImportarCookiesPrivadosToolStripMenuItem.Size = New Size(211, 22)
+        ImportarCookiesPrivadosToolStripMenuItem.Text = "Importar cookies privados"
+        ' 
+        ' AlterarPastaDestinoToolStripMenuItem
+        ' 
+        AlterarPastaDestinoToolStripMenuItem.Name = "AlterarPastaDestinoToolStripMenuItem"
+        AlterarPastaDestinoToolStripMenuItem.Size = New Size(211, 22)
+        AlterarPastaDestinoToolStripMenuItem.Text = "Alterar pasta destino"
+        ' 
+        ' btCancelar
+        ' 
+        btCancelar.BackColor = Color.DarkOrange
+        btCancelar.Cursor = Cursors.Hand
+        btCancelar.FlatAppearance.BorderSize = 0
+        btCancelar.FlatStyle = FlatStyle.Flat
+        btCancelar.ForeColor = Color.White
+        btCancelar.Location = New Point(310, 187)
+        btCancelar.Name = "btCancelar"
+        btCancelar.Size = New Size(102, 30)
+        btCancelar.TabIndex = 20
+        btCancelar.Text = "Cancelar"
+        btCancelar.UseVisualStyleBackColor = False
+        ' 
+        ' Form1
+        ' 
+        ClientSize = New Size(516, 460)
+        Controls.Add(btCancelar)
+        Controls.Add(chkLegendas)
+        Controls.Add(btLog)
+        Controls.Add(StatusStrip1)
+        Controls.Add(MenuStrip1)
+        Controls.Add(CheckBoxAudio)
+        Controls.Add(progressBarDownload)
+        Controls.Add(btLimparLista)
+        Controls.Add(lstLinks)
+        Controls.Add(txtUrl)
+        Controls.Add(btnAdicionar)
+        Controls.Add(btnExecutar)
+        Controls.Add(txtLog)
+        FormBorderStyle = FormBorderStyle.FixedSingle
+        MainMenuStrip = MenuStrip1
+        MaximizeBox = False
+        Name = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "MasterDownloader"
+        MenuStrip1.ResumeLayout(False)
+        MenuStrip1.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Private WithEvents txtUrl As TextBox
+    Private WithEvents btnAdicionar As Button
+    Private WithEvents btnExecutar As Button
+    Private WithEvents txtLog As TextBox
+    Friend WithEvents lstLinks As ListBox
+    Private WithEvents btLimparLista As Button
+    Friend WithEvents progressBarDownload As ProgressBar
+    Friend WithEvents CheckBoxAudio As CheckBox
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents btLog As Button
+    Friend WithEvents chkLegendas As CheckBox
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents ConfoguraçõesToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ImportarCookiesPrivadosToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AlterarPastaDestinoToolStripMenuItem As ToolStripMenuItem
+    Private WithEvents btCancelar As Button
+End Class
---
+++ MasterDownloader/Form1.vb
@@ -0,0 +1,212 @@
+﻿Imports System.Diagnostics
+Imports System.IO
+Imports System.Text.RegularExpressions
+Imports System.Threading.Tasks
+
+Public Class Form1
+
+    Private downloadFilePath As String = Application.StartupPath & "\download.txt" ' pode ser caminho absoluto se preferir
+    Private batFilePath As String = Application.StartupPath & "\run.bat" ' pode ser caminho absoluto se necessário
+
+    Private Sub btnAdicionar_Click(sender As Object, e As EventArgs) Handles btnAdicionar.Click
+        Dim link As String = txtUrl.Text.Trim()
+        If link <> "" Then
+            File.AppendAllText(downloadFilePath, link & Environment.NewLine)
+            lstLinks.Items.Add(link)
+            txtUrl.Clear()
+        End If
+    End Sub
+
+
+    Public Async Function ExecutarProcessoAsync(ByVal logTextBox As TextBox, ByVal progressBar As ProgressBar) As Task(Of Boolean)
+        Dim tcs As New TaskCompletionSource(Of Boolean)()
+        Dim hasErrors As Boolean = False
+
+        progressBar.Invoke(Sub() progressBar.Value = 0)
+
+        Dim psi As New ProcessStartInfo()
+
+        psi.FileName = IO.Path.Combine(Application.StartupPath, "app", "yt-dlp.exe")
+
+        Dim argumentos As New System.Text.StringBuilder()
+        argumentos.Append("--batch-file ""download.txt"" ")
+        If CheckBoxAudio.Checked Then
+            argumentos.Append("--extract-audio --audio-format mp3 ")
+        End If
+        If chkLegendas.Checked Then
+            argumentos.Append("--write-sub --sub-langs ""pt.*"" --embed-subs ")
+        End If
+        argumentos.Append("--output ""downloaded\%(title)s.%(ext)s"" ")
+        argumentos.Append("--ignore-errors ")
+        argumentos.Append("--cookies ""cookies.txt"" ")
+        argumentos.Append("--format ""bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best""")
+
+
+        psi.Arguments = argumentos.ToString()
+
+        psi.WorkingDirectory = Application.StartupPath
+        psi.UseShellExecute = False
+        psi.RedirectStandardOutput = True
+        psi.RedirectStandardError = True
+        psi.CreateNoWindow = True
+
+        Dim proc As New Process()
+        proc.StartInfo = psi
+        proc.EnableRaisingEvents = True
+
+        ' Handler para a saída padrão (output)
+        AddHandler proc.OutputDataReceived, Sub(s, ev)
+                                                If ev.Data IsNot Nothing Then
+                                                    logTextBox.Invoke(Sub() logTextBox.AppendText(ev.Data & Environment.NewLine))
+                                                    If ev.Data.Contains("[download] Destination:") Then
+                                                        progressBar.Invoke(Sub() progressBar.Value = 0)
+                                                    End If
+
+                                                    ' Tenta extrair o progresso da linha de saída
+                                                    Dim match As Match = Regex.Match(ev.Data, "\[download\]\s+(\d{1,3}(?:\.\d+)?)%")
+                                                    If match.Success Then
+                                                        Dim percentText = match.Groups(1).Value.Replace(",", ".")
+                                                        Dim progressVal As Integer = CInt(Math.Floor(Double.Parse(percentText, Globalization.CultureInfo.InvariantCulture)))
+                                                        progressVal = Math.Min(progressVal, 100)
+                                                        progressBar.Invoke(Sub()
+                                                                               If progressVal <= progressBar.Maximum Then
+                                                                                   progressBar.Value = progressVal
+                                                                               End If
+                                                                           End Sub)
+                                                    End If
+                                                End If
+                                            End Sub
+
+        ' Handler para a saída de erro (error)
+        AddHandler proc.ErrorDataReceived, Sub(s, ev)
+                                               If ev.Data IsNot Nothing AndAlso Not String.IsNullOrWhiteSpace(ev.Data) Then
+                                                   hasErrors = True
+                                                   logTextBox.Invoke(Sub() logTextBox.AppendText("[ERRO] " & ev.Data & Environment.NewLine))
+                                               End If
+                                           End Sub
+
+        ' Handler para quando o processo for finalizado
+        AddHandler proc.Exited, Sub(s, ev)
+                                    tcs.TrySetResult(Not hasErrors)
+                                End Sub
+
+        Try
+            proc.Start()
+            proc.BeginOutputReadLine()
+            proc.BeginErrorReadLine()
+        Catch ex As Exception
+            logTextBox.AppendText("[FALHA CRÍTICA] Não foi possível iniciar o processo: " & ex.Message)
+            tcs.TrySetResult(False)
+        End Try
+
+        Return Await tcs.Task
+    End Function
+
+    Private Sub LimparArquivoDownload()
+        Dim caminho As String = Path.Combine(Application.StartupPath, "download.txt")
+        Try
+            File.WriteAllText(caminho, String.Empty)
+            txtLog.AppendText(Environment.NewLine & "🧹 Arquivo download.txt limpo com sucesso!" & Environment.NewLine)
+            lstLinks.Items.Clear() ' Se estiver usando ListBox para mostrar os links
+        Catch ex As Exception
+            MessageBox.Show("Erro ao limpar o arquivo: " & ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Async Function ValidarLink(url As String) As Task(Of Boolean)
+        Dim psi As New ProcessStartInfo With {
+        .FileName = "app\yt-dlp.exe",
+        .Arguments = $"--simulate {url}",
+        .UseShellExecute = False,
+        .RedirectStandardError = True,
+        .RedirectStandardOutput = True,
+        .CreateNoWindow = True
+    }
+
+        Dim output As String = ""
+        Using proc As Process = Process.Start(psi)
+            output = Await proc.StandardError.ReadToEndAsync()
+            proc.WaitForExit()
+        End Using
+
+        Return Not output.Contains("ERROR:")
+    End Function
+
+    Private Async Function ContarVideosNaPlaylist(url As String) As Task(Of Integer)
+        Dim psi As New ProcessStartInfo With {
+        .FileName = "app\yt-dlp.exe",
+        .Arguments = $"--flat-playlist --print ""%(title)s"" {url}",
+        .UseShellExecute = False,
+        .RedirectStandardOutput = True,
+        .CreateNoWindow = True
+    }
+
+        Dim count As Integer = 0
+        Using proc As Process = Process.Start(psi)
+            While Not proc.StandardOutput.EndOfStream
+                Await proc.StandardOutput.ReadLineAsync()
+                count += 1
+            End While
+            proc.WaitForExit()
+        End Using
+        Return count
+    End Function
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If File.Exists(downloadFilePath) Then
+            Dim links = File.ReadAllLines(downloadFilePath)
+            lstLinks.Items.AddRange(links)
+        End If
+        progressBarDownload.Location = New Point(12, 222)
+        Me.Height = 315
+    End Sub
+
+    Private Sub btLimparLista_Click(sender As Object, e As EventArgs) Handles btLimparLista.Click
+        LimparArquivoDownload()
+    End Sub
+    Private Async Sub btnExecutar_Click(sender As Object, e As EventArgs) Handles btnExecutar.Click
+        txtLog.Clear()
+        btnExecutar.Enabled = False
+        progressBarDownload.Value = 0 ' Garante que a barra de progresso esteja zerada ao iniciar
+
+        Try
+            Dim sucesso As Boolean = Await ExecutarProcessoAsync(txtLog, progressBarDownload)
+
+            If sucesso Then
+                txtLog.AppendText(Environment.NewLine & "✅ Arquivos baixados com sucesso!" & Environment.NewLine)
+                Dim pastaDestino As String = IO.Path.Combine(Application.StartupPath, "downloaded")
+                If IO.Directory.Exists(pastaDestino) Then
+                    Process.Start("explorer.exe", pastaDestino)
+                Else
+                    txtLog.AppendText("⚠️ Pasta de destino 'downloaded' não encontrada." & Environment.NewLine)
+                End If
+            Else
+                txtLog.AppendText(Environment.NewLine & "❌ O processo foi concluído com erros." & Environment.NewLine)
+            End If
+
+        Catch ex As Exception
+            txtLog.AppendText(Environment.NewLine & $"[ERRO INESPERADO] {ex.Message}")
+        Finally
+            btnExecutar.Enabled = True
+            progressBarDownload.Value = 0 ' Reseta a barra de progresso ao finalizar
+        End Try
+    End Sub
+    Private Sub btLog_Click(sender As Object, e As EventArgs) Handles btLog.Click
+        If txtLog.Visible Then
+            btLog.Text = "Exibir Log"
+            txtLog.Visible = False
+            progressBarDownload.Location = New Point(12, 222)
+            Me.Height = 315
+        Else
+            txtLog.Visible = True
+            txtLog.Location = New Point(12, 222)
+            progressBarDownload.Location = New Point(12, 404)
+            Me.Height = 500
+            btLog.Text = "Ocultar Log"
+        End If
+
+    End Sub
+
+
+End Class
